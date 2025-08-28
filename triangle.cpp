3788153158e5efddbdb1baed8b10560789b82022
DIVYANSHU MISHRA <mishraaman4512@gmail.com>
2025-08-28 22:38:07 +0530

    vj b

--- triangle.cpp
+++ triangle.cpp
@@ -5,15 +5,28 @@ int main()
     int n=4;
     int num=1;
 
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<n-i-1;j++){
+    //         cout<<" ";
+    //     }
+    //     for(int j=1;j<=i+1;j++){
+    //         cout<<j;
+    //     }
+    //     for(int j=i;j>=1;j--){
+    //         cout<<j;
+    //     }
+    //     cout<<"\n";
+    // }
+
     for(int i=0;i<n;i++){
         for(int j=0;j<n-i-1;j++){
             cout<<" ";
         }
         for(int j=1;j<=i+1;j++){
-            cout<<j;
+            cout<<"*";
         }
         for(int j=i;j>=1;j--){
-            cout<<j;
+            cout<<"*";
         }
         cout<<"\n";
     }
