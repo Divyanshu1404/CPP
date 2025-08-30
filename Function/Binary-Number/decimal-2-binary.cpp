e7710cafa79311ff4f3c6ab4b052c5442c831f11
DIVYANSHU MISHRA <mishraaman4512@gmail.com>
2025-08-30 20:45:32 +0530

    ff

--- Function/Binary-Number/decimal-2-binary.cpp
+++ Function/Binary-Number/decimal-2-binary.cpp
@@ -1,21 +1,23 @@
 #include<iostream>
 using namespace std;
  
-int dec2binary(int n){
+int dec2binary(int decNum){
     int ans=0; 
     int pow=1;
     while(decNum > 0){
         int rem=decNum %2;
-        int decNum =decNum/2;
-        int rem=rem+(rem*pow);
+        decNum =decNum/2;
+        ans=rem+(rem*pow);
         pow=pow*10;
-    }
+    }l
     return ans;
 }
 int main()
 {
-    int decNum;
-    dec2binary(decNum);
-    cout<<"binary number : "<<dec2binary<<endl;
+     int decNum;
+     cout<<"enter the number : ";
+     cin>>decNum; 
+    // dec2binary(decNum);
+    cout<<"binary number : "<<dec2binary(decNum)<<endl;
     return 0;
 }
