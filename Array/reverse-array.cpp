bbff334b14ab0dbb45c4b71437905d5b6eeb2a3b
DIVYANSHU MISHRA <mishraaman4512@gmail.com>
2025-09-01 16:51:46 +0530

    jmn

--- Array/reverse-array.cpp
+++ Array/reverse-array.cpp
@@ -5,10 +5,14 @@ void arrReverse(int arr[],int size){
     int start=0;
     int end=size-1;
     while(start<end){
-        swap(arr[start],arr[end]);
-        start++;
-        end--;
+    //     swap(arr[start],arr[end]);
+    //     start++;
+    //     end--;
+    // }
+    for(int i=0;i<size/2;i++){
+        swap(arr[i],arr[size-i-1]);
     }
+
 }
 
 int main()
--- Array/tempCodeRunnerFile.cpp
+++ Array/tempCodeRunnerFile.cpp
@@ -1,34 +1,4 @@
-
-// #include <iostream>
-// #include <climits>
-// using namespace std;
-
-// // Function to find smallest element in array
-// int findMin(int arr[], int n) {
-//     int smallest = INT_MAX;
-//     for (int i = 0; i < n; i++) {
-//         if (arr[i] < smallest) {
-//             smallest = arr[i];
-//         }
-//     }
-//     return smallest;
-// }
-
-// int main() {
-//     int size;
-//     cout << "Enter the size of array: ";
-//     cin >> size;
-
-//     int marks[size];   // works in GCC/Clang (Variable Length Array), 
-//                        // not standard C++ but supported by many compilers
-
-//     cout << "Enter the values of array: ";
-//     for (int i = 0; i < size; i++) {
-//         cin >> marks[i];
-//     }
-
-//     int smallest = findMin(marks, size);
-
-//     cout << "Smallest: " << smallest << endl;
-//     return 0;
-// }
+    //     swap(arr[start],arr[end]);
+    //     start++;
+    //     end--;
+    // }
