0a529d8f5303fa80d297eb210521a020b542139c
DIVYANSHU MISHRA <mishraaman4512@gmail.com>
2025-09-01 16:57:34 +0530

    vkb

--- Array/reverse-array.cpp
+++ Array/reverse-array.cpp
@@ -5,13 +5,11 @@ void arrReverse(int arr[],int size){
     int start=0;
     int end=size-1;
     while(start<end){
-    //     swap(arr[start],arr[end]);
-    //     start++;
-    //     end--;
-    // }
-    for(int i=0;i<size/2;i++){
-        swap(arr[i],arr[size-i-1]);
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
     }
+    
 
 }
 
@@ -33,4 +31,5 @@ int main()
     }
     cout<<endl;
     return 0;
+
 }
