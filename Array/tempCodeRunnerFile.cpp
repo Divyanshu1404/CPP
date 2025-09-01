04c616d6c7374a855ba0d3b67e76561323cd5385
DIVYANSHU MISHRA <mishraaman4512@gmail.com>
2025-09-01 14:11:26 +0530

    gbed

--- Array/tempCodeRunnerFile.cpp
+++ Array/tempCodeRunnerFile.cpp
@@ -1,30 +1,3 @@
-#include<iostream>
-#include <climits>
-using namespace std;
- 
-int main()
-{
-    int size;
-     cout<<"enter the size of array : ";
-    cin>>size;
-    int marks[size];    
-    cout<<"enter the values of array : ";
-    for (int i=0; i<size;i++){
-        cin>>marks[i];
-    }
-    int smallest=INT_MAX;
-    int index=-1;
-    for(int i=0;i<size;i++){
-        if(marks[i]<smallest){
-            smallest=marks[i];
-            index=i;
-        }
-    }
-    cout<<"Smallest : "<<smallest<<endl;
-    cout<<"index : "<<index<<endl;
-    return 0;
-}
-
 
 // #include <iostream>
 // #include <climits>
