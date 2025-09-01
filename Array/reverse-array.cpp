6b26c9d4c22e1faf2f12266aa9c26e89390a16ef
DIVYANSHU MISHRA <mishraaman4512@gmail.com>
2025-09-01 16:48:03 +0530

    dhj

--- Array/reverse-array.cpp
+++ Array/reverse-array.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
 using namespace std;
 
-int arrReverse(int arr[],int size){
-    int start0;
+void arrReverse(int arr[],int size){
+    int start=0;
     int end=size-1;
-       for (int i=0; i<size;i++){
-       
+    while(start<end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
     }
 }
 
@@ -20,8 +22,11 @@ int main()
     for (int i=0; i<size;i++){
         cin>>arr[i];
     }
+    arrReverse(arr,size);
 
-    cout<<"Reverse Array : "<<arrReverse()<<endl;
-
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
     return 0;
 }
