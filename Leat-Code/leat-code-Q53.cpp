fa45013f3ea2e1a8d24ec99bfcd360e0f81bb7bf
DIVYANSHU MISHRA <mishraaman4512@gmail.com>
2025-09-02 16:12:06 +0530

    hgjk

--- Leat-Code/leat-code-Q53.cpp
+++ Leat-Code/leat-code-Q53.cpp
@@ -16,33 +16,3 @@ public:
     }
 };
 
-
-// #include <iostream>
-// #include <vector>
-// using namespace std;
-
-// class Solution {
-// public:
-//     int maxSubArray(vector<int>& nums) {
-//         int sum = 0;
-//         int maxi = nums[0];
-//         for (int i = 0; i < nums.size(); i++) {
-//             sum = sum + nums[i];
-//             maxi = max(maxi, sum);
-
-//             if (sum < 0)
-//                 sum = 0;
-//         }
-//         return maxi;
-//     }
-// };
-
-// int main() {
-//     Solution sol;
-//     vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-
-//     int result = sol.maxSubArray(nums);
-//     cout << "Maximum Subarray Sum = " << result << endl;
-
-//     return 0;
-// }
