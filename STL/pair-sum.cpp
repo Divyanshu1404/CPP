4e5197611f93c705dcf0dcc7595aedb36c16a4ff
DIVYANSHU MISHRA <mishraaman4512@gmail.com>
2025-09-02 17:20:12 +0530

    flio

--- STL/pair-sum.cpp
+++ STL/pair-sum.cpp
@@ -6,22 +6,44 @@ using namespace std;
 vector<int> pairSum(vector<int>nums,int target){
     vector<int>ans;
     int n=nums.size();
-    for (int i=0;i<n;i++){
-        for(int j=i;j<n;j++){
-            if(nums[i]+nums[j]==target){
-                ans.push_back(i);
-                ans.push_back(j);
-                return ans;
+    // for (int i=0;i<n;i++){
+    //     for(int j=i;j<n;j++){
+    //         if(nums[i]+nums[j]==target){
+    //             ans.push_back(i);
+    //             ans.push_back(j);
+    //             return ans;
 
-            }
-        } 
+    //         }
+    //     } 
+    // }
+
+
+    //Using while loop and more optimised ans
+    // this is only applicable when the array is in shorted form
+
+    int i=0;
+    int j=n-1;
+
+    while(i<j){
+        int pairSum=nums[i]+nums[j];
+        if(pairSum > target){
+            j--;
+        }
+        else if(pairSum < target){
+            i++;
+        }
+        else{
+            ans.push_back(i);
+            ans.push_back(j);
+            return ans;
+        }
     }
     return ans;
 }
 int main()
 {
-    vector<int>nums={1,7,6,8,9,3};
-    int target=17;
+    vector<int>nums={1,2,3,4,8,9,10};
+    int target=7;
 
 
     vector<int>ans =pairSum(nums ,target);
