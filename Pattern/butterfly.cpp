2ef9dbeead4ce992df2c9e557c7e1573b4b8b14c
DIVYANSHU MISHRA <mishraaman4512@gmail.com>
2025-09-15 13:15:31 +0530

    iu

--- Pattern/butterfly.cpp
+++ Pattern/butterfly.cpp
@@ -1,32 +1,40 @@
-#include<iostream>
-using namespcace std;
+#include <iostream>
+using namespace std;
 
-int main()
-{
-    int n=5;
-    int spaces=2*n-;
-    int stars=0;
+int main() {
+    int n = 5;
+    int spaces = 2 * n - 1;
+    int stars = 0;
 
-    for (int i=1;i<=2*n-1;i++){
-        if(n<=n
-{
-    spaces=spaces-1;
-    stars++;
-}   
-    else{
-        spaces=spaces+2;
-        stars++;
-    } 
-    for(int j=1;j<=spaces;j++){
-        cout<<"*";
+    for (int i = 1; i <= 2 * n - 1; i++) {
+        // Upper half
+        if (i <= n) {
+            spaces =spaces - 2;
+            stars++;
+        }
+        // Lower half
+        else {
+            spaces += 2;
+            stars--;
+        }
+
+        // Left stars
+        for (int j = 1; j <= stars; j++) {
+            cout << "*";
+        }
+
+        // Middle spaces
+        for (int j = 1; j <= spaces; j++) {
+            cout << " ";
+        }
+
+        // Right stars
+        for (int j = 1; j <= stars; j++) {
+            cout << "*";
+        }
+
+        cout << "\n";
     }
-    for(int j=1;j<=stars;j++){
-        cout<<" ";
-    }
-    for(intj=1;j<=stars;j++){
-        if()
-    }
-}
 
-
+    return 0;
 }
