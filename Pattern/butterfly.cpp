87862999197b938f8ec432e3cabc40c97bb37626
DIVYANSHU MISHRA <mishraaman4512@gmail.com>
2025-09-15 13:05:21 +0530

    ih

---
+++ Pattern/butterfly.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespcace std;
+
+int main()
+{
+    int n=5;
+    int spaces=2*n-;
+    int stars=0;
+
+    for (int i=1;i<=2*n-1;i++){
+        if(n<=n
+{
+    spaces=spaces-1;
+    stars++;
+}   
+    else{
+        spaces=spaces+2;
+        stars++;
+    } 
+    for(int j=1;j<=spaces;j++){
+        cout<<"*";
+    }
+    for(int j=1;j<=stars;j++){
+        cout<<" ";
+    }
+    for(intj=1;j<=stars;j++){
+        if()
+    }
+}
+
+
+}
