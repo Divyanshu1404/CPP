8af17f30149be7547e820e0694fc57a966be1cc3
DIVYANSHU MISHRA <mishraaman4512@gmail.com>
2025-11-01 14:05:14 +0530

    bi

--- Array/sub-array.cpp
+++ Array/sub-array.cpp
@@ -26,6 +26,7 @@ int main()
     return 0;
 }
 
+
 // #include <iostream>
 // #include <vector>
 // using namespace std;
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,8 +1,1 @@
-#include<iostream>
-using namespace std;
  
-int main()
-{
-    cout<<sizeof(long int);
-    return 0;
-}
