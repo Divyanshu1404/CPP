0cf6cfbb9b4569f7823eff662c14a38166c90d27
DIVYANSHU MISHRA <mishraaman4512@gmail.com>
2025-11-24 22:11:04 +0530

    Kadans algo

--- Leat-Code/leat-code-Q53.cpp
+++ Leat-Code/leat-code-Q53.cpp
@@ -1,18 +1,24 @@
-//using Kadans algorithm trying to find the maximum subarray. 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
 
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int sum=0;
-        int maxi=nums[0];
-        for(int i=0;i<nums.size();i++){
-            sum=sum+nums[i];
-            maxi=max(maxi,sum);
+        int sum = 0;        // Current subarray sum
+        int maxi = nums[0]; // Maximum sum found so far
 
-            if(sum<0)
-            sum=0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum = sum + nums[i];           // Add current element to running sum
+            maxi = max(maxi, sum);         // Update max if current sum is larger
+
+            // If sum becomes negative, it's better to start fresh from next element
+            if (sum < 0)
+                sum = 0;
         }
         return maxi;
     }
 };
-
