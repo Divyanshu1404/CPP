2c70389d9c53261c30d30aefd1ce42e35433f65a
DIVYANSHU MISHRA <mishraaman4512@gmail.com>
2025-12-12 14:07:49 +0530

    level2

--- Pattern/level2.cpp
+++ Pattern/level2.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main() {
-    int n = 5;
+    int n = 3;
     for (int i=n/2;i>=0;i--) {
-        for (int j=1;j<i+(n/2)+1;j++) {
+        for (int j=1;j<i+(n/2)+2;j++) {
             cout<<" ";
         }
         for (int j=n-(2*i);j>0;j--) {
@@ -14,9 +14,9 @@ int main() {
     }
         
         for (int i=0;i<n;i++){
-            for (int j=1;j<2*n+1;j++){
+            for (int j=0;j<2*n+2;j++){
                 if(j==n){
-                    cout<<"@";
+                    cout<<"+";
                 }
                 else{
                     cout<<" ";
@@ -24,11 +24,10 @@ int main() {
             }
             cout<<endl;
         }
-        for (int i=0;i<5;i++){
-            for (int j=0;j<2*n-2;j++){
-                if(j==(n/2)-1||j==2*n-(n/2+1)||i==0 && j>=n/2&&j<2*n-n/2){
+        for (int i=0;i<n+1;i++){
+            for (int j=0;j<(2*n)+1;j++){
+                if(j==(n/2)||j==2*n-(n/2)||i==0 && j>=n/2&&j<2*n-n/2){
                     cout<<"*";
-
                 }
                 else{
                     cout<<" ";
@@ -36,7 +35,6 @@ int main() {
             }
             cout<<endl;
         }
-      
         for (int i=0; i<n/2+1;i++){
             for (int j=0;j<n*2+1;j++){
                 if(j>=i&&j<n-i || j>=n+1+i && j<=n*2-i){
--- Pattern/tempCodeRunnerFile.cpp
+++ Pattern/tempCodeRunnerFile.cpp
@@ -1,3 +1,1 @@
-for(int i=0;i<;i++){
-    //     for()
-    // }
+    for (int i=0;i<n/2+1;i++){
