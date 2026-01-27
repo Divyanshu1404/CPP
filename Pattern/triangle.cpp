7f945fcd921e28dc7b540c8d2f8b5f5e87661598
DIVYANSHU MISHRA <mishraaman4512@gmail.com>
2026-01-27 11:20:09 +0530

    3poi

--- Pattern/triangle.cpp
+++ Pattern/triangle.cpp
@@ -2,32 +2,28 @@
 using namespace std;
 int main()
 {
-    int n=4;
-
-    // for(int i=0;i<n;i++){
-    //     for(int j=0;j<n-i-1;j++){
-    //         cout<<" ";
-    //     }
-    //     for(int j=1;j<=i+1;j++){
-    //         cout<<j;
-    //     }
-    //     for(int j=i;j>=1;j--){
-    //         cout<<j;
-    //     }
-    //     cout<<"\n";
-    // }
-
+    int n;
+    cout<<"enter the number";
+    cin>>n;
     for(int i=0;i<n;i++){
         for(int j=0;j<n-i-1;j++){
             cout<<" ";
         }
-        for(int j=1;j<=i+1;j++){
-            cout<<"*";
-        }
-        for(int j=i;j>=1;j--){
+        for(int j=0;j<2*i+1;j++){
             cout<<"*";
         }
         cout<<"\n";
     }
+    //  Reverse triangle
+    for(int i=n-2;i>=0;i--){
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<2*i+1;j++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    
     return 0;
 }
