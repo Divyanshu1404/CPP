fe8523eb2cbbc32a18df26557f451c93cc0ab13b
DIVYANSHU MISHRA <mishraaman4512@gmail.com>
2026-03-16 23:05:26 +0530

    vdfs

--- oops/class.cpp
+++ oops/class.cpp
@@ -1,17 +1,43 @@
 #include<iostream>
-#include "hero.cpp"
 using namespace std;
 
 // creation of class
-class  hero{
-   char name;
-   int health;
-   
+class hero{
+private:
+    int health;
+
+public:
+    char level;
+
+    void print(){
+        cout<<level<<endl;
+    }
+
+    int gethealth(){
+        return health;
+    }
+
+    char getlevel(){
+        return level;
+    }
+
+    void sethealth(int h){
+        health = h;
+    }
+
+    void setlevel(char ch){
+        level = ch;
+    }
 };
+
 int main(){
-    // creation of object
     hero h1;
 
-    cout<<"size : "<<sizeof(h1)<<endl;
+    h1.sethealth(100);
+    h1.level = 'A';
+
+    cout<<"health is : "<<h1.gethealth()<<endl;
+    cout<<"level is : "<<h1.level<<endl;
+
     return 0;
 }
