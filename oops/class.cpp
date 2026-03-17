e7ac56b278b6e0ebc28e56a53daa2ca974704530
DIVYANSHU MISHRA <mishraaman4512@gmail.com>
2026-03-17 21:55:15 +0530

    if

--- oops/class.cpp
+++ oops/class.cpp
@@ -31,13 +31,42 @@ public:
 };
 
 int main(){
-    hero h1;
 
-    h1.sethealth(100);
-    h1.level = 'A';
 
-    cout<<"health is : "<<h1.gethealth()<<endl;
-    cout<<"level is : "<<h1.level<<endl;
 
-    return 0;
+    
+//        hero a;
+//    a.sethealth(80);
+//    a.setlevel('B');
+//    cout<<"level is : "<<a.level<<endl;
+//    cout<<"health is :"<<a.gethealth()<<endl;
+
+// dynamic allocation
+
+//    hero *b=new hero;
+
+//    b->sethealth(70);
+//    b->setlevel('A');
+//    cout<<"level is : "<<(*b).level<<endl;
+//    cout<<"health is :"<<(*b).gethealth()<<endl;
+
+//    cout<<"level is : "<<b->level<<endl;
+//    cout<<"health is :"<<b->gethealth()<<endl;
+
+//    return 0;
+
+   //  hero h1;
+   //  h1.sethealth(100);
+   //  h1.level = 'A';
+
+   //  cout <<"size of hero : "<<sizeof(h1)<<endl;
+
+   //  cout<<"health is : "<<h1.gethealth()<<endl;
+   //  cout<<"level is : "<<h1.level<<endl;
+
+   //  return 0;
+   
+   // static allocation
+
+
 }
